c4acae5108d4f472ea8c25a069976a66bea74598
Norman209 <147957226+Norman209@users.noreply.github.com>
2024-04-27 15:40:23 -0700

    Update website_server_v2.py

--- Website/website_server_v2.py
+++ Website/website_server_v2.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, send_from_directory, jsonify,redirect,render_template,send_file
+from flask import Flask, request, send_from_directory, jsonify,redirect,render_template,send_file,make_response
+from werkzeug.utils import secure_filename
+import logging
+log = logging.getLogger('pydrop')
 import shutil
 import os
 import uuid
 app = Flask(__name__, static_folder='public')
 app.config['UPLOAD_FOLDER']='/Users/milesnorman/websites/upload_folders'
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+upload_directory='/Users/milesnorman/websites/upload_folders'
 print('starting server')
 
 
@@ -17,13 +21,13 @@ def starting_page():
     return render_template('augment.html')
 
 
-#USING FETCH API WITH JS AND PYTHON
+# USING FETCH API WITH JS AND PYTHON
 
 # // let formData = new FormData(); // empty data 
 # // let image_id='random id here'  // id that will be assigned to data
 
 # // dataset = document.getElementById("folder_upload_id").files; //client's uploaded folder from html form
-
+ 
 # // sample_image=dataset[0] //first image of client's uploaded folder
 
 # // formData.append('image',sample_image) // adding image to data that will be posted to flask server
@@ -48,47 +52,60 @@ def starting_page():
 @app.route('/download/<filename>', methods=['GET']) #<filename> is a dynamic parameter, meaning it's not a fixed value and is the text after '/download'
 def download(filename):
 
-    print('filename:',filename) #in this case, this statement will output 'name_of_file_you_want_to_download' when the anchor tag is clicked on
-
+    print('filename:',filename)
     directory = '/Users/milesnorman/websites/upload_folders' # Specify the directory where your file is stored
     
     # Return the file to the client for download
     return send_file(os.path.join(directory, filename), as_attachment=True)
 
 
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    file = request.files['file']
+
+    save_path = os.path.join(upload_directory, secure_filename(file.filename))
+    current_chunk = int(request.form['dzchunkindex'])
+
+    # If the file already exists it's ok if we are appending to it,
+    # but not if it's new file that would overwrite the existing one
+    if os.path.exists(save_path) and current_chunk == 0:
+        # 400 and 500s will tell dropzone that an error occurred and show an error
+        return make_response(('File already exists', 400))
+
+    try:
+        with open(save_path, 'ab') as f:
+            f.seek(int(request.form['dzchunkbyteoffset']))
+            f.write(file.stream.read())
+    except OSError:
+        # log.exception will include the traceback so we can see what's wrong 
+        log.exception('Could not write to file')
+        return make_response(("Not sure why," " but we couldn't write the file to disk", 500))
+
+    total_chunks = int(request.form['dztotalchunkcount'])
+
+    if current_chunk + 1 == total_chunks:
+        # This was the last chunk, the file should be complete and the size we expect
+        if os.path.getsize(save_path) != int(request.form['dztotalfilesize']):
+            log.error(f"File {file.filename} was completed, "
+                      f"but has a size mismatch."
+                      f"Was {os.path.getsize(save_path)} but we"
+                      f" expected {request.form['dztotalfilesize']} ")
+            return make_response(('Size mismatch', 500))
+        else:
+            log.info(f'File {file.filename} has been uploaded successfully')
+    else:
+        log.debug(f'Chunk {current_chunk + 1} of {total_chunks} '
+                  f'for file {file.filename} complete')
+
+    return make_response(("Chunk upload successful", 200))
+
+
+
+
 @app.route('/Classification',methods=['POST','GET']) 
 def Classification():
-    if request.method=='POST':
-        #print('is this null?',request.files['hello'])
-        #value=request.form['folder']
-        #f = request.files['folder']  # Handle multiple files
-        image = request.files['image'] # Get list of files 
-        dataset_id=request.form['id']
-        keys=request.form.keys()
-        if 'start_id' in keys:
-            if not os.path.exists(f'/Users/milesnorman/websites/upload_folders/{dataset_id}'):
-                upload_folder_path=f'/Users/milesnorman/websites/upload_folders/{dataset_id}'
-                os.mkdir(upload_folder_path)
-        image.save(os.path.join(f'/Users/milesnorman/websites/upload_folders/{dataset_id}', image.filename))
-        if 'end_id' in keys:
-            #augment dataset then zip it
-            print('ZIPPING DATASET...')
-            shutil.make_archive(f'/Users/milesnorman/websites/upload_folders/{dataset_id}', 'zip', dataset_id)
-            print('DONE ZIPPING',dataset_id)
-        #print('saved image successfully')
-        #print('files:',files)
-        #current_chunk = int(request.form['dzchunkindex'])
-        #print('current chunk:',current_chunk)
-        # print('stream:',f)
-        # for key in f.keys():
-        #     for value in f.getlist(key):
-        #         print(key,":",value)
-        if 'end_id' in keys:
-            return render_template('Classification.html') #test='test'
-        else:
-            return render_template('Classification.html')
-    else:
-        return render_template('Classification.html')
+    return render_template('Classification.html')
 
 
 @app.route('/Object_detection',methods=['POST','GET'])
